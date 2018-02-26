d776a68dbd7ee3ec80edca8d582478eaddb7ef95
wangzhe12 <wangzhe12@baidu.com>
2018-02-26 20:07:31 +0800

    修复文章8中存在的bug

--- 8multi_hidden_nn/multi_hidden_function.py
+++ 8multi_hidden_nn/multi_hidden_function.py
@@ -58,7 +58,8 @@ def initialize_parameters_deep(layer_dims):
     L = len(layer_dims)  # number of layers in the network
 
     for l in range(1, L):
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) / np.sqrt(layer_dims[l - 1])
+        # parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
         parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
         assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
         assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
